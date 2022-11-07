950942841d873f09aa7bcea477ff67133c9d2969
EunmiHwang <cncn6666@naver.com>
2022-11-07 10:19:49 +0900

    [feature] Tobi / Zohnny / Jinny / HennaActivity로 이동하기 위한 버튼 액션 추가하기 (#2)

--- app/src/main/java/com/kakaovx/practice/myapplication/MainActivity.kt
+++ app/src/main/java/com/kakaovx/practice/myapplication/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.kakaovx.practice.myapplication
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<TextView>(R.id.tv_tobi).apply {
+            startActivity(Intent(context, TobiActivity::class.java))
+        }
+
+        findViewById<TextView>(R.id.tv_zohnny).apply {
+            startActivity(Intent(context, ZohnnyActivity::class.java))
+        }
+
+        findViewById<TextView>(R.id.tv_jinny).apply {
+            startActivity(Intent(context, JinnyActivity::class.java))
+        }
+
+        findViewById<TextView>(R.id.tv_henna).apply {
+            startActivity(Intent(context, HennaActivity::class.java))
+        }
     }
 }
