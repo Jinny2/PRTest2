18a099fba6bb71818f532ded0577f580ce734ca8
Henna <henna.hyen97@kakaovx.com>
2022-11-07 11:17:51 +0900

    [feature] xml에 있는 버튼과 텍스트뷰를 연결하기 위해 findViewById를 사용한다. (#9)
    findViewById로 요소 찾기

--- app/src/main/java/com/kakaovx/practice/myapplication/HennaActivity.kt
+++ app/src/main/java/com/kakaovx/practice/myapplication/HennaActivity.kt
@@ -1,9 +1,14 @@
 package com.kakaovx.practice.myapplication
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class HennaActivity : AppCompatActivity() {
+    private val btn = findViewById<Button>(R.id.btn_hello)
+    private val txt = findViewById<TextView>(R.id.tv_hello)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_henna)
